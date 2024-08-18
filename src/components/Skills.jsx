f5c07984b7a27c1a52f541ff05a4c21dffec3067
amin-khorram <amin.khorram.ca@gmail.com>
2024-08-18 19:00:38 -0400

    added the skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,94 +1,87 @@
 import React from "react";
 
-import html from "../assets/html.png";
-import css from "../assets/css.png";
-import javascript from "../assets/javascript.png";
-import reactImage from "../assets/react.png";
-import nextjs from "../assets/nextjs.png";
-import graphql from "../assets/graphql.png";
+// Importing images from the assets folder
+import python from "../assets/python.png";
+import numpy from "../assets/numpy.png";
+import pandas from "../assets/pandas.png";
+import scikitLearn from "../assets/scikit-learn.png";
+import tensorflow from "../assets/tensorflow.png";
+import pytorch from "../assets/pytorch.png";
+import jupyter from "../assets/jupyter.png";
+import sql from "../assets/sql.png";
+import aws from "../assets/aws.png";
+import docker from "../assets/docker.png";
+import git from "../assets/git.png";
 import github from "../assets/github.png";
-import tailwind from "../assets/tailwind.png";
-import bootstrap from "../assets/bootstrap.png"
-import git from "../assets/git.png"
-import visualstudio from "../assets/visualstudio.png"
-import python from "../assets/python.png"
-import nodejs from "../assets/node.png"
 
 const Skills = () => {
+  // Array of technology objects with image source and title
   const techs = [
     {
       id: 1,
-      src: html,
-      title: "HTML",
-    },
-    {
-      id: 2,
-      src: css,
-      title: "CSS",
-    },
-    {
-      id: 3,
-      src: javascript,
-      title: "JavaScript",
-    },
-    {
-      id: 4,
-      src: reactImage,
-      title: "React",
-    },
-    {
-      id: 5,
-      src: tailwind,
-      title: "Tailwind",
-    },
-    {
-      id: 6,
-      src: nextjs,
-      title: "Next JS",
-    },
-    {
-      id: 7,
-      src: graphql,
-      title: "GraphQL",
-    },
-    {
-      id: 8,
-      src: github,
-      title: "GitHub",
-    },
-    {
-      id: 9,
-      src: bootstrap,
-      title: "Bootstrap",
-    },
-    {
-      id: 10,
-      src: git,
-      title: "Git",
-    },
-    {
-      id: 11,
-      src: visualstudio,
-      title: "Visual Studio",
-    },
-    {
-      id: 12,
       src: python,
       title: "Python",
     },
     {
-      id: 13,
-      src: nodejs,
-      title: "Node Js",
+      id: 2,
+      src: numpy,
+      title: "NumPy",
+    },
+    {
+      id: 3,
+      src: pandas,
+      title: "Pandas",
+    },
+    {
+      id: 4,
+      src: scikitLearn,
+      title: "Scikit-Learn",
+    },
+    {
+      id: 5,
+      src: tensorflow,
+      title: "TensorFlow",
+    },
+    {
+      id: 6,
+      src: pytorch,
+      title: "PyTorch",
+    },
+    {
+      id: 7,
+      src: jupyter,
+      title: "Jupyter",
+    },
+    {
+      id: 8,
+      src: sql,
+      title: "SQL",
+    },
+    {
+      id: 9,
+      src: aws,
+      title: "AWS",
+    },
+    {
+      id: 10,
+      src: docker,
+      title: "Docker",
+    },
+    {
+      id: 11,
+      src: git,
+      title: "Git",
+    },
+    {
+      id: 12,
+      src: github,
+      title: "GitHub",
     },
   ];
 
   return (
-    <div
-      name="Skills"
-      className=" w-full h-screen"
-    >
-      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full ">
+    <div name="Skills" className="w-full h-screen">
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Skills
@@ -102,7 +95,7 @@ const Skills = () => {
               key={id}
               className="hover:scale-125 duration-500 py-2 rounded-lg"
             >
-              <img src={src} alt="" className="w-20 mx-auto" />
+              <img src={src} alt={title} className="w-20 mx-auto" />
               <p className="mt-4">{title}</p>
             </div>
           ))}
